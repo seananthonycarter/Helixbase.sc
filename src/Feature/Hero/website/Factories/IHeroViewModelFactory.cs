cabfeeb9781fda5c5e613a405248fb37b7591ce4
Sean Carter <sean.carter@sagittarius.agency>
2023-06-17 15:47:51 +0100

    Fist commit

---
+++ src/Feature/Hero/website/Factories/IHeroViewModelFactory.cs
@@ -0,0 +1,10 @@
+﻿using Helixbase.Feature.Hero.Models;
+using Helixbase.Feature.Hero.ViewModels;
+
+namespace Helixbase.Feature.Hero.Factories
+{
+    public interface IHeroViewModelFactory
+    {
+        HeroViewModel CreateHeroViewModel(IHeroContentType heroItemDataSource, bool isExperienceEditor);
+    }
+}
---
+++ src/Feature/Hero/website/Services/IHeroService.cs
@@ -0,0 +1,12 @@
+﻿using Helixbase.Feature.Hero.Models;
+using Helixbase.Foundation.Search.Models;
+
+namespace Helixbase.Feature.Hero.Services
+{
+    public interface IHeroService
+    {
+        IHeroContentType GetHeroItems();
+        BaseSearchResultItem GetHeroImagesSearch();
+        bool IsExperienceEditor { get; }
+    }
+}
